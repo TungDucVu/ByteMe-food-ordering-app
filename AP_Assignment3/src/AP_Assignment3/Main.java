5df4bb40b2fc25a6074089236522c73926756d9d
TungDucVu <147933411+TungDucVu@users.noreply.github.com>
2024-11-23 22:19:19 +0530

    Update Main.java

--- AP_Assignment3/src/AP_Assignment3/Main.java
+++ AP_Assignment3/src/AP_Assignment3/Main.java
@@ -1,62 +1,30 @@
 package AP_Assignment3;
 
-import java.util.Scanner;
 
 public class Main {
-	AdminScreen AdminScreen;
-	CustomerScreen CustomerScreen;
-	Scanner scanner;
-	Customer VIP;
-	Customer Normal;
-	private static final String FILE_NAME_VIP = "VIP.ser";
-	private static final String FILE_NAME_NORMAL = "Normal.ser";
+	LoginScreenUI LoginScreenUI;
+    AdminScreenUI AdminScreenUI;
+	CustomerScreenUI CustomerScreenUI;	
 	
-	
-	Main() throws FoodItemNotFoundException, OrderNotFoundException {
-		AdminScreen = new AdminScreen();
-		CustomerScreen = new CustomerScreen();
-		scanner = new Scanner(System.in);
-		VIP = Customer.loadFromFile(FILE_NAME_VIP);
-		Normal = Customer.loadFromFile(FILE_NAME_NORMAL);
-		
-		int role = chooseRole();
-		if (role == 1) {
-			AdminScreen.printAdmin();
-		}
-		else if (role == 2) {
-			CustomerScreen.printCustomerScreen(VIP);
-		}
-		else if (role == 3) {
-			CustomerScreen.printCustomerScreen(Normal);
-		}
-		
-	}
-	
-	private int chooseRole() {
-		System.out.println("****************************************");
-		System.out.println("1. ADMIN");
-		System.out.println("2. VIP CUSTOMER");
-		System.out.println("3. NORMAL CUSTOMER");
-		System.out.println("4. EXIT");
-		int choice;
-		do {
-            while (!scanner.hasNextInt()) {
-                System.out.println("Invalid input. Please enter a number between 1 and 4.");
-                scanner.next();
-            }
-            choice = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
+	Main(){
+		LoginScreenUI = new LoginScreenUI();
+		Customer toLogin = LoginScreenUI.getLogin();
 
-            if (choice < 1 || choice > 4) {
-                System.out.println("Invalid choice. Please enter a number between 1 and 4.");
-            }
-        } while (choice < 1 || choice > 4);
-		return choice;
+		if (LoginScreenUI.getAdmin()) {
+		    // Admin login successful
+			AdminScreenUI = new AdminScreenUI();
+		} else if (toLogin != null) {
+		    // Customer login successful
+			CustomerScreenUI = new CustomerScreenUI(toLogin);
+		} else {
+		    // Handle case if both admin and customer login failed (optional)
+		    System.out.println("Login failed.");
+		}
+
+
 	}
-	
-	
-	
-	public static void main(String[] args) throws FoodItemNotFoundException, OrderNotFoundException {
+
+	public static void main(String[] args){
 		new Main();
 	}
 }
